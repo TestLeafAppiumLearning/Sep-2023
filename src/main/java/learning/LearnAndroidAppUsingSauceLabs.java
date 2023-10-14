a1dc91f01c08517a3a84a716d29df831eec834b7
Lokesh Kumar Thyagarajan <LokeshKumar.Thyagarajan@hidglobal.com>
2023-10-14 18:16:50 +0530

    POM and Sauce lab code added

--- src/main/java/learning/LearnAndroidAppUsingSauceLabs.java
+++ src/main/java/learning/LearnAndroidAppUsingSauceLabs.java
@@ -1,6 +1,37 @@
 package learning;
 
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.MutableCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
 public class LearnAndroidAppUsingSauceLabs {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
+        MutableCapabilities caps = new MutableCapabilities();
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("appium:app", "storage:filename=leaforg.apk");  // The filename of the mobile app
+        caps.setCapability("appium:deviceName", "Android GoogleAPI Emulator");
+        caps.setCapability("appium:platformVersion", "13.0");
+        caps.setCapability("appium:automationName", "UiAutomator2");
+        MutableCapabilities sauceOptions = new MutableCapabilities();
+        sauceOptions.setCapability("appiumVersion", "2.0.0");
+        sauceOptions.setCapability("username", "oauth-epcmafather-5bc49");
+        sauceOptions.setCapability("accessKey", "3108bbd1-4ffc-4834-8536-5e25e5c6872c");
+        sauceOptions.setCapability("build", "<your build id>");
+        sauceOptions.setCapability("name", "<your test name>");
+        sauceOptions.setCapability("deviceOrientation", "PORTRAIT");
+        caps.setCapability("sauce:options", sauceOptions);
+
+        URL url = new URL("https://ondemand.eu-central-1.saucelabs.com:443/wd/hub");
+        AndroidDriver driver = new AndroidDriver(url, caps);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.findElement(AppiumBy.className("android.widget.EditText")).sendKeys("rajkumar@testleaf.com");
+        driver.findElement(AppiumBy.xpath("(//android.widget.EditText)[2]")).sendKeys("Leaf@123");
+        driver.findElement(AppiumBy.className("android.widget.Button")).click();
+        Thread.sleep(5000);
+        driver.quit();
     }
 }
